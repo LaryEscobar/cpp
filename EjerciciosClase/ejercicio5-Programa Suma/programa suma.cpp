50ce257a5f76a1c343195bddba3df5dca998e7f3
Alma Escobar <amayalaryssa97@gmail.com>
2022-01-31 21:30:54 -0600

    ejercicio5

---
+++ EjerciciosClase/ejercicio5-Programa Suma/programa suma.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+
+int a = 0;
+int b = 0;
+int resultado;
+
+cout <<"Ingrese el primer valor: " <<endl;
+cin >> a;
+
+cout <<" Ingrese el segundo valor: " <<endl;
+cin >> b;
+
+
+resultado = a + b ;
+
+cout << "La suma es: " << resultado << endl;
+ 
+
+
+		
+system("pause");
+return 0;
+}
