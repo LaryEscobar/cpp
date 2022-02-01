d91b6f6f974a80ee34ce0ad07427bd65f1476a61
Alma Escobar <amayalaryssa97@gmail.com>
2022-01-31 21:08:00 -0600

    ejercicio4

---
+++ EjerciciosClase/ejercicio4-imprimir variables/todas-variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+int main (){
+
+bool valorbolean;
+valorbolean = true;
+
+int entero = 15;
+
+double vdouble =20.99;
+
+string vstring = "Hola uwu";
+
+char vchar = 'b';
+
+cout << "Valor Bolean: " << valorbolean << endl;
+cout << "Valor Entero: " << entero << endl;
+cout << "Valor Double: " << vdouble << endl;
+cout << "Valor String: " << vstring << endl;
+cout << "Valor Char: " << vchar<< endl;
+
+
+		
+system("pause");
+return 0;
+}
