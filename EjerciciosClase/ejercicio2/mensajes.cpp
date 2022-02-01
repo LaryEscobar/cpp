70cf8eb2ab2c95a4426c41db3303f916b507c73e
Alma Escobar <amayalaryssa97@gmail.com>
2022-01-31 19:07:49 -0600

    ejercicio2

---
+++ EjerciciosClase/ejercicio2/mensajes.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+    string nombre ;
+
+    cout << "Ingrese su nombre: " <<endl;
+    cin >> nombre;
+
+	
+ cout << "Hola mi nombre es: " << nombre <<endl; 
+	    
+		
+system("pause");
+return 0;
+}
