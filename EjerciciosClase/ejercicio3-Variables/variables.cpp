b10e3f99b816c473e449ba5114ea1f2e59507d8e
Alma Escobar <amayalaryssa97@gmail.com>
2022-01-31 19:58:36 -0600

    ejercicio3

---
+++ EjerciciosClase/ejercicio3-Variables/variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+
+int entero;
+
+entero = 15;
+cout << "Entero: "<< entero << endl;
+
+entero = 18 ;
+cout << "Entero Modificado: "<< entero <<endl;
+		
+system("pause");
+return 0;
+}
