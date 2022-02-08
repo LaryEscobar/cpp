ef02880af98e8f5479242fbc26daa14b3b76d511
Alma Escobar <amayalaryssa97@gmail.com>
2022-02-07 21:45:56 -0600

    cambio2

--- EjerciciosClase/ejercicio15-Notas completas/notasc.cpp
+++ EjerciciosClase/ejercicio15-Notas completas/notasc.cpp
@@ -1,4 +1,4 @@
-	#include <iostream> 
+#include <iostream> 
 	#include<stdio.h>
 	
 	using namespace std;
@@ -16,88 +16,88 @@
 	
 	}  else {
 	
-	         if (nota >= 5 && nota <= 9 ){
+	        if (nota >= 5 && nota <= 9 ){
 	        cout <<" Obtuvo una  F- " <<endl;	
 	       } else {
-	              if (nota >=10 && nota <= 14){
-	               cout <<" Obtuvo una F " <<endl;	
-	               } else {
-	               	      if (nota >= 15 && nota <= 19 ){
-							cout <<" Obtuvo una F " <<endl;	
+	            if (nota >=10 && nota <= 14){
+	             cout <<" Obtuvo una F " <<endl;	
+	            } else {
+	                if (nota >= 15 && nota <= 19 ){
+					cout <<" Obtuvo una F " <<endl;	
+					} else {
+						if (nota >= 20  && nota <= 24 ){
+						cout <<" Obtuvo una F+" <<endl;
+						} else { 
+						    if (nota >= 25 && nota <= 29 ){
+							cout <<" Obtuvo una E- " <<endl;
 							} else {
-							    if (nota >= 20  && nota <= 24 ){
-							    cout <<" Obtuvo una F+" <<endl;
-							    } else { 
-								      if (nota >= 25 && nota <= 29 ){
-									  cout <<" Obtuvo una E- " <<endl;
-									  } else {
-									        if (nota >= 30 && nota <= 34 ){
-											cout <<" Obtuvo una E " <<endl;
+								if (nota >= 30 && nota <= 34 ){
+								cout <<" Obtuvo una E " <<endl;
+								} else {
+									if (nota >= 35 && nota <= 39 ){
+									cout <<" Obtuvo una E+ " <<endl;
+									} else { 
+										if (nota >= 40 && nota <= 44 ){
+										cout <<" Obtuvo una D- " <<endl;
+										}else {
+										   if (nota >= 45 && nota <= 49 ){
+											cout <<" Obtuvo una D " <<endl;
 											} else {
-											    if (nota >= 35 && nota <= 39 ){
-												cout <<" Obtuvo una E+ " <<endl;
-												} else { 
-												    if (nota >= 40 && nota <= 44 ){
-													cout <<" Obtuvo una D- " <<endl;
-										           }else {
-												        if (nota >= 45 && nota <= 49 ){
-														cout <<" Obtuvo una D " <<endl;
-														} else {
-														    if (nota >= 50 && nota <= 54 ){
-															cout <<" Obtuvo una D+ " <<endl;
-															} else {
-															    if (nota >= 55 && nota <= 59 ){
-																cout <<" Obtuvo una C- " <<endl;
+												if (nota >= 50 && nota <= 54 ){
+												cout <<" Obtuvo una D+ " <<endl;
+												} else {
+													if (nota >= 55 && nota <= 59 ){
+													cout <<" Obtuvo una C- " <<endl;
+													} else {
+														if (nota >= 60 && nota <= 64 ){
+														cout <<" Obtuvo una C  " <<endl;
+														}  else {
+														    if (nota >= 65 && nota <= 69 ){
+															cout <<" Obtuvo una C+ " <<endl;
+															}else {
+																if (nota >= 70 && nota <= 74 ){
+																cout <<" Obtuvo una B- " <<endl;
 																} else {
-																    if (nota >= 60 && nota <= 64 ){
-																	cout <<" Obtuvo una C  " <<endl;
-																	}  else {
-																	    if (nota >= 65 && nota <= 69 ){
-																		cout <<" Obtuvo una C+ " <<endl;
-																		}else {
-																			if (nota >= 70 && nota <= 74 ){
-																				cout <<" Obtuvo una B- " <<endl;
+																	if (nota >75 && nota <= 79){
+																	cout << " Obtuvo una B " <<endl;
+																	} else {
+																		if (nota >80 && nota <= 84){
+																		cout << " Obtuvo una B+ " <<endl;
+																		} else {
+																			if (nota >85 && nota <= 89){
+																			cout << " Obtuvo una A- " <<endl;
 																			} else {
-																			if (nota >75 && nota <= 79){
-																			cout << " Obtuvo una B " <<endl;
-																			} else {
-																				if (nota >80 && nota <= 84){
-																				cout << " Obtuvo una B+ " <<endl;
+																				if (nota >90 && nota <= 94){
+																				cout << " Obtuvo una A " <<endl;
 																				} else {
-																				if (nota >85 && nota <= 89){
-																				cout << " Obtuvo una A- " <<endl;
-																				 } else {
-																					if (nota >90 && nota <= 94){
-																					cout << " Obtuvo una A " <<endl;
-																					} else {
-																					    if (nota >95 && nota <= 99){
-																						cout << " Obtuvo una A+ " <<endl;
-																					    }else {
-																					        if (nota = 100){
-																						    cout << " Obtuvo una A++ " <<endl;
-																					         } else {
-																					            cout << "Ingrese un valor de 0 a 100" <<endl;
-																								 }
-																							}
-																						}
-																				    }
-																			    }
-																		   }
-																	    }
+																					if (nota >95 && nota <= 99){
+																					cout << " Obtuvo una A+ " <<endl;
+																					}else {
+																						if (nota = 100){
+																						 cout << " Obtuvo una A++ " <<endl;
+																					    } else {
+																					cout << "Ingrese un valor de 0 a 100" <<endl;
+																				 }
+																			}
+																		}
 																	}
-							                                    }           
-					                                        } 
-				                                        }
-		                                            }
-	                                            }
-                                            }
-	                                    }
-	                                }	
-								}
-							}
-						}
-					}
+															    }
+															}
+													    }
+												    }
+							                    }           
+					                       } 
+				                        }
+		                            }
+	                            }
+                            }
+	                    }
+	                }	
 				}
+			}
+	    }
+   }
+}
 	
 	
 	system("pause");
