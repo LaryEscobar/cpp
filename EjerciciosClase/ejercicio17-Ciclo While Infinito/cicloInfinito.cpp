dccfbd45013593c3faf14e3806c1830b03475778
Alma Escobar <amayalaryssa97@gmail.com>
2022-02-10 11:47:42 -0600

    ejercicio 17

---
+++ EjerciciosClase/ejercicio17-Ciclo While Infinito/cicloInfinito.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+	
+	int ciclos = 0 ;
+
+ 
+while (ciclos < 1000 ){
+	cout << " * ";
+	
+	ciclos = ciclos + 1;
+	
+}
+	    
+		
+system("pause");
+return 0;
+}
