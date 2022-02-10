46fe02fb89311a631d000b9393c61ae0369a8ace
Alma Escobar <amayalaryssa97@gmail.com>
2022-02-10 11:05:56 -0600

    ejercicio16

---
+++ EjerciciosClase/ejercicio16-Ciclo While/ciclowhile.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+ 
+ int a = 5;
+ int b = 2;
+ 
+ while (a > b){
+ 	
+ 	cout <<" Ingrese valor de a " << endl;
+ 	cin >> a;
+ } 
+ 
+ 
+ cout << endl;
+ cout <<" Salio del ciclo " <<endl; 
+	    
+		
+system("pause");
+return 0;
+}
