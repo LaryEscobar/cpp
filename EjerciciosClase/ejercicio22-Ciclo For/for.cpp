cacc54b2a85651e8a50019299ec819f7eeeca266
Alma Escobar <amayalaryssa97@gmail.com>
2022-02-10 15:55:05 -0600

    ejercicio22

---
+++ EjerciciosClase/ejercicio22-Ciclo For/for.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+	
+	
+	for (int indice =1; indice <= 10; indice ++){
+		
+		cout <<"Valor de la variable indice " << indice << endl;
+		
+	}
+		
+system("pause");
+return 0;
+}
