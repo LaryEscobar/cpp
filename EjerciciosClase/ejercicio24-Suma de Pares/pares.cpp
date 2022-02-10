177a1efb88cf35c653e5f9764714e5c0d7c52abe
Alma Escobar <amayalaryssa97@gmail.com>
2022-02-10 16:53:24 -0600

    ejercicio24

---
+++ EjerciciosClase/ejercicio24-Suma de Pares/pares.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+#include<stdio.h>
+
+using namespace std;
+
+int main (){
+	
+	int pares = 0;
+	
+	for (int i =1; i <= 10; i ++){
+		
+		if (i%2 == 0){
+			
+			pares = pares + i ;
+		}
+		
+		cout << i << "  ";
+		
+	}
+
+cout << endl;
+cout << endl;
+cout <<" Total pares: " << pares <<endl;
+system("pause");
+return 0;
+}
