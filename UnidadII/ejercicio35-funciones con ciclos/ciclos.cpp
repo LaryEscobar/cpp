3a00f8f893ebfa097dc93e622779eacb34e051ae
Alma Escobar <amayalaryssa97@gmail.com>
2022-03-09 17:39:38 -0600

    ejercicio35

---
+++ UnidadII/ejercicio35-funciones con ciclos/ciclos.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+void imprimircaracteres( char caracter, int ciclos)
+{
+    for (int i = 0; i < ciclos; i++)
+    {
+        cout << caracter;
+    }
+    
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    imprimircaracteres ('@', 10);
+    imprimircaracteres ('#', 5);
+    cout << " Hola mundo"<< endl;
+    imprimircaracteres ('*', 1000);
+    return 0;
+}
+
