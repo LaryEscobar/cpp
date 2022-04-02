e530087ecb8890c4ce7f36571fc5e7c13a58ab98
Alma Escobar <amayalaryssa97@gmail.com>
2022-04-01 19:23:03 -0600

    menu

--- Proyecto/Sistema De Carcel/menu.cpp
+++ Proyecto/Sistema De Carcel/menu.cpp
@@ -6,8 +6,37 @@ void menu (){
 
     int opcion ;
 
+    while (true)
+    {
+        system ("cls");
 
+
+        cout << " Bienvenido, Penitenciaria Colon " << endl ;
+        cout << " ----------- INICIO ------------ " << endl;
+        cout << " 1. Inserta Recluso " << endl ;
+        cout << " 2. Modificar Recluso " << endl ;
+        cout << " 3. Eliminar Recluso " << endl ;
+        cout << " 4. Visualizar Reclusos " << endl ;
+        cout << " 0. Salir " << endl ;
+
+
+        cout << " Agregue opcion: " << endl ;
+        cin >> opcion ;
+
+        if (opcion == 0)
+        {
+            break;
+        }
+        
+
+
+
+
+    }
     
+
+
+
 
 
 
