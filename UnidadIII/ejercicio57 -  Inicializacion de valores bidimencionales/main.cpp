061a466e3a1d29d7ad40b165a0919dba0328a2d5
Alma Escobar <amayalaryssa97@gmail.com>
2022-04-04 22:38:44 -0600

    unidadIII

--- UnidadIII/ejercicio57 -  Inicializacion de valores bidimencionales/main.cpp
+++ UnidadIII/ejercicio57 -  Inicializacion de valores bidimencionales/main.cpp
@@ -4,20 +4,30 @@ using namespace std ;
 
 int main(int argc, char const *argv[])
 {
-    string nombres [3][2];
+   // string nombres [3][2];
 
-    nombres [ 0][0] = "Alma";
-    nombres [ 0][1] = "Amaya";
+  // nombres [ 0][0] = "Alma";
+  // nombres [ 0][1] = "Amaya";
 
-    nombres [ 1][0] = "Angie";
-    nombres [ 1][1] = "Escobar";
+  //  nombres [ 1][0] = "Angie";
+  //  nombres [ 1][1] = "Escobar";
 
-    nombres [ 2][0] = "Lourdes";
-    nombres [ 2][1] = "Villanueva";
+  //  nombres [ 2][0] = "Lourdes";
+  //  nombres [ 2][1] = "Villanueva";
 
-    for (int i = 0; i < 3 ; i++)
+  int fila = 3;
+  int columna = 2;
+
+      string nombres [fila][columna] ={
+        {"Alma","Perez"},
+        {"Angie","Escobar"},
+        {"Lourdes","Perez"},
+
+      };
+  
+    for (int i = 0; i < fila ; i++)
     {
-      for (int j = 0; j < 2; j++)
+      for (int j = 0; j < columna ; j++)
       {
            cout << nombres [i][j] << " ";
       }
